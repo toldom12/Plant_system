c25cba21bfae5a54df3d553376428d366ef124da
xyz <michal.gadzinski1@gmail.com>
2024-04-23 21:17:38 +0200

    add: draft case 1

--- PlantController.py
+++ PlantController.py
@@ -2,13 +2,13 @@ from Plant import Plant
 
 
 class PlantController:
-
     def __init__(self):
         self.plants = []
 
     def add_plant(self,
                   plant: Plant):
         self.plants.append(plant)
+        pass
 
     def water_plant(self, plant_name: str,
                     amount: float):
--- PlantWateringSystem.py
+++ PlantWateringSystem.py
@@ -7,9 +7,18 @@ def Menu():
         match response:
             case 0:
                 print(f'Show plant list')
-                
             case 1:
-                print(f'Add plant to table')
+                print(f'Create a [PLANT]')
+                plant_name: str = str(input(f'Give a name of plant'))
+                info_about_water_lvl: float = float(input(f' info about water lvl of plant: {plant_name}'))
+                new_plant = Plant(name=plant_name,
+                                  water_level=info_about_water_lvl)
+                name_of_plant, info_lvl_water = new_plant.base_info_plant()
+                plant = PlantController()
+                if isinstance(name_of_plant, str):
+                    plant.add_plant(plant=new_plant)
+                    pass
+
             case 2:
                 print(f'Write expected  level of water plant in [mm]')
             case 3:
@@ -25,9 +34,3 @@ def Menu():
 if __name__ == '__main__':
     Menu()
     pass
-
-    # controller = PlantController()
-    # plant_1 = Plant(name="tuja",
-    #                 water_level=50)
-    # controller.add_plant(plant=plant_1)
-    # print(controller.plants)
