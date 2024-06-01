c0cb0bdc8812db08b9443743e90d09696957c381
xyz <michal.gadzinski1@gmail.com>
2024-06-01 14:11:41 +0200

    add: operating on I/O files

--- PlantController.py
+++ PlantController.py
@@ -20,9 +20,19 @@ class PlantController:
 
     def save_plant_to_file_txt(self, plant: Plant):
         with open(self.path, mode='a+') as self.file:
-            self.file.write(f'Plant_name:  {plant.name},  Plant amount of water: {plant.water_level}\n')
+            print(f'-------------------------------------------------')
+            self.file.write(f'Plant_name:  {plant.name} , Plant amount of water: {plant.water_level}\n')
+            print(f'-------------------------------------------------')
 
-    def read_plant_with_file_txt(self, index: int = 0):
+    def read_names_of_plants_with_file_txt(self):
+        with open(self.path, mode='r') as self.file:
+            lines = self.file.readlines()
+            for index, row in enumerate(lines):
+                row = row.split()
+                expected_name = row[1].replace(',', '')
+                print(index, expected_name)
+
+    def read_plant_info_with_file_txt(self, index: int = 0):
         name = input(str(f'Write name of plant'))
         if isinstance(name, str):
             try:
@@ -33,14 +43,28 @@ class PlantController:
                         line = row.split()
                         return_value = line[1].find(name)
                         if return_value == 0:
-                            print(row)
-                            break
+                            return row
                         elif len(lines) == index and return_value != 0:
-                            raise Plant_not_found_name_in_file()
+                            return None
                         elif return_value != 0:
                             pass
-            except Plant_not_found_name_in_file as e:
-                print(f'Error: {e}')
+            except IOError as y:
+                print(f'ERROR : {y}')
+
+    def find_expected_data_about_plant(self) -> bool:
+        try:
+            expected_row = self.read_plant_info_with_file_txt()
+            if not isinstance(expected_row, str):
+                raise Plant_not_found_name_in_file()
+            expected_row = expected_row.split()
+            name = expected_row[1].replace(',', '')
+            amount_of_water = float(expected_row[6])
+            print(f'name: {name}')
+            print(f'Amount of water: {amount_of_water}')
+            return True
+        except Plant_not_found_name_in_file as f:
+            print(f'ERROR : {f} ')
+            return False
 
     def water_plant(self, plant_name: str,
                     amount: float):
@@ -51,6 +75,6 @@ class PlantController:
         pass
 
 
-# a = PlantController()
-# a.read_plant_with_file_txt()
-# pass
+a = PlantController()
+a.read_names_of_plants_with_file_txt()
+pass
--- PlantWateringSystem.py
+++ PlantWateringSystem.py
@@ -3,10 +3,17 @@ def Menu():
     from PlantController import PlantController
     print(f'Hello in Plant Watering system  !! ')
     while True:
+        print(f'Show plant list')
+        print(f'-----------------')
+        print(f' 0 - Show list of the [PLANTS]')
+        print(f' 1 - Create a [PLANT]')
+        print(f' 2 - Get Info about expected [PLANT]')
+        print(f'-----------------')
         response = int(input(f'Write value : '))
         match response:
             case 0:
-                print(f'Show plant list')
+                p = PlantController()
+                p.read_names_of_plants_with_file_txt()
             case 1:
                 print(f'Create a [PLANT]')
                 plant_name: str = str(input(f'Give a name of plant'))
@@ -21,18 +28,26 @@ def Menu():
                     pass
 
             case 2:
-                print(f'Write expected  level of water plant in [mm]')
-                pass 
+                p = PlantController()
+                result = p.find_expected_data_about_plant()
+                print(f'result in find file : {result}')
+
             case 3:
-                print(f'Check plant humidity')
+                print(f'Write expected  level of water plant in [mm]')
+                pass
             case 4:
-                print(f'fill the plant')
+                print(f'Check plant humidity')
             case 5:
+                print(f'fill the plant')
+            case 6:
                 print(f'delete plant with list')
             case _:
                 break
 
 
 if __name__ == '__main__':
-    Menu()
-    pass
+    from PlantController import Plant_not_found_name_in_file
+    try:
+        Menu()
+    except Exception as e:
+        print(f'An unexpected error occured {e}')
